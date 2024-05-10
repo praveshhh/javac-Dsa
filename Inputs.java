537bcc26b7a77b0a40274d764bf9ce1e7963bd74
praveshhh <83650338+praveshhh@users.noreply.github.com>
2024-05-10 08:13:42 +0000

    making two variable and make them work together to give single output by taking inn Inputs from users config/done

---
+++ Inputs.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Inputs{
+    public static void main(String [] args){
+        Scanner input= new Scanner(System.in);
+        System.out.print("enter the star war code: " );
+        int code = input.nextInt();
+        System.out.println("your code is " + code);
+
+    }
+} 
