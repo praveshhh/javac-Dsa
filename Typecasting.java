4dff955fec01279ea06697e66faa4d9d22a27ec6
praveshhh <83650338+praveshhh@users.noreply.github.com>
2024-05-10 14:28:47 +0000

    type casting : int num = (int) (67.56f)=> output: 67 config done/

---
+++ Typecasting.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Typecasting {
+    public static void main (String [] args){
+        Scanner input = new Scanner(System.in);
+        //float num = input.nextFloat();
+
+        int num = (int) (67.56f);
+        System.out.println(num);
+    }
+}
