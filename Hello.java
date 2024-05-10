ce997456240297315d4d8c7394ec71f9e3cf4035
praveshhh <83650338+praveshhh@users.noreply.github.com>
2024-05-10 08:01:39 +0000

    .next , .nextLine , .... config done/

--- Hello.java
+++ Hello.java
@@ -3,7 +3,11 @@ public class Hello{
     public static void main(String []args){
         System.out.println("hello world");
         Scanner input = new Scanner(System.in);
-        System.out.println(input.nextInt());
+       // System.out.println(input.nextInt());//taking only integer value
+        // System.out.println(input.next()); takin input only single string 
+        System.out.println(input.nextLine());
+        
+
 
     }
 }
