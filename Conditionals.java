c99ce4c8d27f9cd1dcfb8c75b8169c2c03be9aaf
praveshhh <83650338+praveshhh@users.noreply.github.com>
2024-05-11 05:52:06 +0000

    if else config done

---
+++ Conditionals.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Conditionals{
+    public static void main (String [] args){
+        int salary = 70000;
+        if (salary > 50000){
+            System.out.println(" you are not poor anymore!");
+
+        }
+        else{
+            System.out.println("poor poor poor! you can cry now");
+            
+        }
+       
+}
+}
