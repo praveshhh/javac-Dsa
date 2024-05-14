6325805b02680711870023ead987ef7755b160dc
praveshhh <83650338+praveshhh@users.noreply.github.com>
2024-05-14 19:06:09 +0000

    fibo done

--- Fibonacci.java
+++ Fibonacci.java
@@ -6,13 +6,13 @@ public class Fibonacci{
         int a= 0;
         int b = 1;
         int count = 2;
-        while (count < = n ){
+        while (count <= n ){
             int temp = b ;
             b = b+a;
             a= temp;
             count ++;
         } 
         System.out.println(b);
-        
+
     }
 } 
