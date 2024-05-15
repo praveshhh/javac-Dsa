ab1e0054774372e7be88aeebabf19663f99dd4f4
praveshhh <83650338+praveshhh@users.noreply.github.com>
2024-05-15 03:04:03 +0000

    1

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,18 +1,51 @@
+// import java.util.Scanner;
+// public class Fibonacci{
+//     public static void main(String args []){
+//         Scanner in = new Scanner(System.in);
+//         int n= in.nextInt();
+//         int a= 0;
+//         int b = 1;
+//         int count = 2;
+//         while (count <= n ){
+//             int temp = b ;
+//             b = b+a;
+//             a= temp;
+//             count ++;
+//         } 
+//         System.out.println(b);
+
+//     }
+// } 
+
+
+
 import java.util.Scanner;
 public class Fibonacci{
-    public static void main(String args []){
+    public static void main(String args []) {
         Scanner in = new Scanner(System.in);
-        int n= in.nextInt();
-        int a= 0;
+        int n = in.nextInt();
+        int a = 0;
         int b = 1;
         int count = 2;
-        while (count <= n ){
-            int temp = b ;
+        while( count <= 2 ){
+            int temp = b;
             b = b+a;
             a= temp;
-            count ++;
-        } 
+            count++ 
+        }
         System.out.println(b);
+        
 
     }
-} 
+}
+
+
+
+
+
+
+
+
+
+
+
