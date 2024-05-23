0e1568b66439dcc286a5bec3ccaefd4b3dfc6734
praveshhh <83650338+praveshhh@users.noreply.github.com>
2024-05-23 05:01:29 +0000

    Array list and array of arraylist config done/

---
+++ ArrayListArray.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+public class ArrayListArray{
+    public static void main (String [] args){
+        Scanner in = new Scanner(System.in);
+        ArrayList <ArrayList <Integer>> list = new ArrayList<> ();
+        // now initialisation took place 
+        for ( int i=0; i<3; i++){
+            list.add (new ArrayList<> ());
+        }
+        // here we add new element 
+        for (int i=0; i<3; i++){
+            for (int j=0; j<3; j++){
+                list.get(i).add(in.nextInt());
+
+            }
+        }
+        System.out.println(list);
+    }
+}
