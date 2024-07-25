8d47d9781277a163c5ca7aa2aa9645f3b708409f
praveshhh <83650338+praveshhh@users.noreply.github.com>
2024-07-25 04:30:54 +0000

    EscapeSequence practicing done/

---
+++ Escapesequence.java
@@ -0,0 +1,9 @@
+public class Escapesequence {
+    public static void main (String [] args){
+       
+        System.out.println("Roses are red,\n\tViolets are blue,\n" +
+"Sugar is sweet,\n\tBut I have \"commitment issues\",\n\t" +
+"So I’d rather just be friends\n\tAt this point in our " +
+"relationship.");
+    }
+}
