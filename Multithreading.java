179f02bc007b3060e5295cd65c009cb72abee703
praveshhh <83650338+praveshhh@users.noreply.github.com>
2024-07-28 06:44:34 +0000

    build the second object to run the second thread lol/

--- Multithreading.java
+++ Multithreading.java
@@ -1,6 +1,8 @@
 public class Multithreading {
     public static void main (String [] args){
         MultithreadingThing myThing = new MultithreadingThing();
+          MultithreadingThing myThing2 = new MultithreadingThing();
         myThing.start();
+         myThing2.start();
     }
 }
